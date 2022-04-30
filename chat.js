f1241124075047e37123593386da62fe9126c381
Konstantin Chukreev <krunch12345@gmail.com>
2022-04-30 11:25:33 +0300

    l6

---
+++ chat.js
@@ -0,0 +1,55 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const socket = require('socket.io')
+
+const HTML_TO_DISPLAY = path.join(path.resolve(), 'chat.html')
+
+const server = http.createServer(
+    (req, res) => {
+        const readStream = fs.createReadStream(HTML_TO_DISPLAY)
+        
+        res.writeHead(200, { 'Content-Type': 'text/html'})
+        
+        readStream.pipe(res)
+    }
+).listen(4444, 'localhost')
+
+const io = socket(server)
+
+io.on(
+    'connection',
+    (client) => {
+        const userCount = io.engine.clientsCount
+        const userName = generateUserName()
+        
+        //пересчитываем количество пользователей в чате
+        client.broadcast.emit('changeUserCount', userCount)
+        client.emit('changeUserCount', userCount)
+        
+        //указываем для пользователя его имя
+        client.emit('setName', userName)
+        
+        //сообщяаем в чат, что зашел новый пользователь
+        client.broadcast.emit('addUser', userName)
+        
+        //сообщяаем в чат, что зашел новый пользователь
+        client.on('disconnect', () => {
+            client.broadcast.emit('leftUser', userName);
+            client.broadcast.emit('changeUserCount', io.engine.clientsCount)
+        })
+        
+        client.on('newMessage', (payload) => {
+            const msg = {
+                user: userName,
+                text: payload
+            }
+            client.broadcast.emit('newChatMessage', msg)
+            client.emit('newChatMessage', msg)
+        })
+    }
+)
+
+const generateUserName = () => {
+    return 'user#'+(Date.now()).toString().substr(-5)
+}
